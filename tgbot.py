94df885c9ac648c3d8e81f211ea6c64504da2c30
Fedor Golishevskii <fedor@dexterenergy.ai>
2024-04-03 17:32:35 +0000

    tgbot add more checks

--- tgbot.py
+++ tgbot.py
@@ -5,7 +5,7 @@ from telegram.ext import filters, MessageHandler
 
 import sys
 import os
-from pursuit import get_closest_to_file
+from pursuit import detect_characters
 from tempfile import NamedTemporaryFile
 
 # logging.basicConfig(
@@ -15,13 +15,17 @@ from tempfile import NamedTemporaryFile
 
 
 async def photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    new_file = await update.message.effective_attachment.get_file()
+    if not update.message or not isinstance(update.message.effective_attachment, list):
+        print("Invalid attachment type", file=sys.stderr)
+        return
+    attachment = update.message.effective_attachment
+    new_file = await attachment[-1].get_file()  # get largest file
     # update.message.text
     try:
         with NamedTemporaryFile(delete=True) as f:
             bs = await new_file.download_as_bytearray()
             f.write(bs)
-            rows = get_closest_to_file(f.name)
+            rows = detect_characters(f.name, 5)
     except Exception as e:
         print(e, file=sys.stderr)
         await context.bot.send_message(
@@ -33,21 +37,21 @@ async def photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
 
 
-async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text="I'm a bot to detect fursuiters, please send a photo to me!",
+        text="I'm a bot that identifies fursuiters in pictures, please send a photo to me!",
     )
 
 
 if __name__ == "__main__":
-    token = os.environ.get("TG_TOKEN")
+    token = os.environ.get("TG_BOT_TOKEN", "")
     application = ApplicationBuilder().token(token).build()
 
     photo_handler = MessageHandler((~filters.COMMAND) & filters.PHOTO, photo)
     application.add_handler(photo_handler)
 
-    help_handler = CommandHandler("help", help)
-    application.add_handler(help_handler)
+    start_handler = CommandHandler("start", start)
+    application.add_handler(start_handler)
 
     application.run_polling()
